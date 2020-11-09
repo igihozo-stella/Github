8964407de10d721340597969d093346dfdab4a87
igihozo-stella <igihozo96@gmail.com>
2020-11-09 18:20:46 +0200

    define the paths for the created routes.

--- Angular-apps/Github/src/app/app-routing.module.ts
+++ Angular-apps/Github/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent} from './profile/profile.component';
+import { GithubComponent} from './github/github.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'github', component: GithubComponent},
+  { path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
